5d84b7f4158d9d1118d85416dd19dc8d5c2a5f35
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-04-13 16:54:57 +0300

    Massive update

--- assets/js/manual-slider.js
+++ assets/js/manual-slider.js
@@ -1,47 +1,22 @@
 class ManualSlider {
-  constructor(el, images = []) {
+  constructor(el) {
 
     if(!el) {
       throw new Error('Specify root selector')
     }
 
-    if(!images.length) {
-      throw new Error('Add images')
-    }
-
     this.el = el;
-    this.images = images;
     this.count = 0;
+    this.images = this.el.querySelectorAll('img');
+    this.images[0].classList.add('current');
 
-    this.init();
-    this.initHTML();
-    this.initEvents();
+    this.initNav();
+    this.navEvents();
   }
 
-  init() {
-   this.el.querySelector("img").src = `${this.images[this.count]}`;
-  }
-
-  initHTML() {
-    let nav = document.createElement('div');
-    nav.classList.add('nav');
-    nav.innerHTML = `
-      <button class="previous"></button>
-      <button class="next"></button>
-    `;
-    this.el.appendChild(nav);
-  }
-
-  initEvents() {
-    let that = this;
-
-    that.el.querySelector('.previous').addEventListener('click', function() {
-      that.slidePrev();
-    });
-
-    that.el.querySelector('.next').addEventListener('click', function() {
-      that.slideNext();
-    });
+  setCurrent() {
+    this.el.querySelector(".current").classList.remove('current');
+    this.images[this.count].classList.add('current')
   }
 
   slidePrev() {
@@ -51,7 +26,7 @@ class ManualSlider {
       this.count = this.images.length - 1;
     }
 
-    this.init(this.images[this.count]);
+    this.setCurrent(this.images[this.count]);
   }
 
   slideNext() {
@@ -61,6 +36,28 @@ class ManualSlider {
       this.count = 0;
     }
 
-    this.init(this.images[this.count]);
+    this.setCurrent(this.images[this.count]);
+  }
+
+  initNav() {
+    let nav = document.createElement('div');
+    nav.classList.add('nav');
+    nav.innerHTML = `
+      <button class="previous"></button>
+      <button class="next"></button>
+    `;
+    this.el.appendChild(nav);
+  }
+
+  navEvents() {
+    let that = this;
+
+    that.el.querySelector('.previous').addEventListener('click', function() {
+      that.slidePrev();
+    });
+
+    that.el.querySelector('.next').addEventListener('click', function() {
+      that.slideNext();
+    });
   }
 }
